bc9657d5785fcfec53ced03b0e0416dad63c14af
tiyang <loutiyang@gmail.com>
2014-03-08 15:42:45 -0600

    Created stories resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Truestory::Application.routes.draw do
+
+  resources :stories
+
   get "welcome/index"
 
   get "welcome/about"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'faker'
 
-rand(1..10).times do
-  p = Story.create(title: Faker::Lorem.words(rand(1..10)).join(" "), body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
+rand(5..10).times do
+  p = Story.create(title: Faker::Lorem.words(rand(1..3)).join(" "), body: Faker::Lorem.paragraphs(rand(1..4)).join("\n"))
   rand(3..10).times do
     p.comments.create(body: Faker::Lorem.paragraphs(rand(1..2)).join("\n"))
   end
