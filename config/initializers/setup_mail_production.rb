a3e5c2d49ffe79d054437dd991c4d7eab05e3e45
tiyang <loutiyang@gmail.com>
2014-03-15 17:02:36 -0500

    Configured app for Production emails

---
+++ config/initializers/setup_mail_production.rb
@@ -0,0 +1,12 @@
+if Rails.env.production?
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address:        'smtp.sendgrid.net',
+    port:           '587',
+    authentication: :plain,
+    user_name:      "app22854083@heroku.com",
+    password:       "dw59gdim",
+    domain:         'heroku.com',
+    enable_starttls_auto: true
+  }
+end
