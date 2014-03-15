8500cdfbdfec7cf36946f442dbb78696e6a6314c
tiyang <loutiyang@gmail.com>
2014-03-15 17:21:12 -0500

    production email

--- config/initializers/setup_mail_production.rb
+++ config/initializers/setup_mail_production.rb
@@ -4,8 +4,8 @@ if Rails.env.production?
     address:        'smtp.sendgrid.net',
     port:           '587',
     authentication: :plain,
-    user_name:      "app22854083@heroku.com",
-    password:       "dw59gdim",
+    user_name:      ENV['SENDGRID_USERNAME'],
+    password:       ENV['SENDGRID_PASSWORD'],
     domain:         'heroku.com',
     enable_starttls_auto: true
   }
